dc853e22fa3bb6a4b57a2848386408421e9dcd62
OwOlr <dbfl8154@gmail.com>
2023-03-28 14:08:42 +0900

    secHand complete

--- Assets/Scripts/AroundBall_C.cs
+++ Assets/Scripts/AroundBall_C.cs
@@ -1,19 +1,19 @@
-using JetBrains.Annotations;
-using OpenCover.Framework.Model;
-using Unity.VisualScripting;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-//[ExecuteInEditMode]
-public class AroundBall : MonoBehaviour
+//[ExecuteInEditMode]   //실행 하기전에 프리뷰에 볼 수 있음.
+
+public class AroundBall_C : MonoBehaviour
 {
-    // Rotation Direction
-    // CW: Clockwise
-    // CCW: Counter Clockwise
+    //Rotation direction
+    //CW : ClockWise 시계방향
+    //CCW: Counter ClockWise 반시계방향
     private enum ERotDir { CW, CCW }
     private enum ERotType { Pitch, Yaw, Roll }
 
     #region Public Variables
-    [Header("- Game Objects -")]
+    [Header("- GameObject -")]
     [SerializeField]
     private GameObject ballPrefab = null;
     [SerializeField]
@@ -22,8 +22,9 @@ public class AroundBall : MonoBehaviour
     [Header("- Values -")]
     [SerializeField, Range(0f, 300f)]
     private float speed = 100f;
+
     [SerializeField, Range(0f, 10f)]
-    private float distance = 1f;    // Radius
+    private float distance = 1f;    //Radius
 
     [Header("- Type -")]
     [SerializeField]
@@ -31,93 +32,102 @@ public class AroundBall : MonoBehaviour
     [SerializeField]
     private ERotType rotType = ERotType.Yaw;
     #endregion
-
-
+    
+    
+    //자식에 있는 Speare 트랜스폼을 가져온다.
     private Transform ballTr = null;
 
     private float angle = 0f;
 
-    public class TestClass {
+    public class TestClass
+    {
         public int i;
     }
-    public struct TestStruct {
+    public struct TestStruct
+    {
         public int i;
     }
     private void Test()
     {
+        //클래스와 구조체의 차이
         TestClass tc = new TestClass();
         tc.i = 1;
         TestClass cpyClass = tc;
         cpyClass.i = 10;
-        Debug.Log("tc.i: " + tc.i);
+        Debug.Log("tc.i = " + tc.i);
 
         TestStruct ts = new TestStruct();
         ts.i = 1;
-        TestStruct cpyStruct = ts;
-        cpyStruct.i = 10;
-        Debug.Log("ts.i: " + ts.i);
+        TestStruct cpystruct = ts;
+        cpystruct.i = 10;
+        Debug.Log("ts.i = " + ts.i); 
     }
+    
 
     private void Awake()
     {
         Test();
-
+        //자식의  transform을 가져옴.
         //ballTr = transform.GetChild(0);
         //Debug.Log(ballTr.name);
-        if (ballPrefab == null)
+        if(ballPrefab == null)
         {
             Debug.LogError("ballPrefab is missing!");
-            return;
         }
-
         GameObject go = Instantiate(ballPrefab);
-        //go.transform.parent = transform;
-        go.transform.SetParent(transform);
+        //go.transform.parent = transform; //최근에 들어 없어진 기능.
+        go.transform.SetParent(transform);  //최근에 기능 추가
         ballTr = go.transform;
     }
-
     private void Update()
     {
+
         if (targetTr == null) return;
 
-        // Clamp
         switch (rotDir)
         {
             case ERotDir.CW:
                 angle -= Time.deltaTime * speed;
                 if (angle < 0f) angle = 360f;
                 break;
+
             case ERotDir.CCW:
+                //1. 각도를 알아야 한다
                 angle += Time.deltaTime * speed;
+                //Clamp 사용 해보기 value, min, max vlaue 값이 min보다 작으면 min값으로 기입, max도 동일한 기능.
                 if (angle > 360f) angle = 0f;
                 break;
         }
-        
 
         Vector3 anglePos = new Vector3();
-        CalcAnglePosWithRotType(
-            rotType, angle, ref anglePos);
+        CalcAnglePosWithType(rotType, angle, ref anglePos);
 
-        Vector3 targetPos = targetTr.position;
-        ballTr.position =
-            targetPos + (anglePos * distance);
+        Vector3 targetPos = targetTr.position;    
+        ballTr.position = targetPos +(anglePos * distance);
+
     }
 
-    private void CalcAnglePosWithRotType(
-        ERotType _rotType,
-        float _angle,
-        ref Vector3 _pos)
+    private void CalcAnglePosWithType(ERotType _rotType, float _angle, ref Vector3 _pos)
     {
+        //Vector3는 구조체이다.
+        //값이 변경 될 때 복사본을 생성하는 데 변경 값을 유지하게 되면 방향이 누적 되어 정확한 방향을 알 수 없다.
+        //C#은 무조건 참조 값(주소) 방식으로 날린다.
+
+        //함수 내부에서 함수 외부의 값 변경을 할 수 있다.
+        //ref - 값을 바꾸지 않아도 된다.
+        //out - 값을 무조건 바꿔야 한다.
+
         float angle2Rad = _angle * Mathf.Deg2Rad;
-        switch(_rotType)
+        switch (_rotType)
         {
             case ERotType.Pitch:
                 _pos.x = 0f;
-                _pos.y = Mathf.Sin(angle2Rad);
-                _pos.z = Mathf.Cos(angle2Rad);
+               _pos.y = Mathf.Sin(angle2Rad);
+               _pos.z = Mathf.Cos(angle2Rad);
                 break;
             case ERotType.Yaw:
-                _pos.x = Mathf.Cos(angle2Rad);
+                _pos.x = Mathf.Cos(angle2Rad); //anlge (X) , Radian(O) -> 호도법으로 계산.
+                //Deg2Rad = Degree(각도) to Radian(호)
                 _pos.y = 0f;
                 _pos.z = Mathf.Sin(angle2Rad);
                 break;
@@ -125,7 +135,12 @@ public class AroundBall : MonoBehaviour
                 _pos.x = Mathf.Cos(angle2Rad);
                 _pos.y = Mathf.Sin(angle2Rad);
                 _pos.z = 0f;
+
                 break;
         }
+
+        
+
     }
+
 }
---
+++ Assets/Scripts/SecHand.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecHand : MonoBehaviour
+{
+   
+    [SerializeField]
+    private GameObject secpPrefab = null;
+    [SerializeField]
+    private Transform targetTr = null;
+
+    [Header("- Values -")]
+    [SerializeField, Range(0f,300f)]
+    private float speed = 10f;
+    [SerializeField, Range(0f,10f)]
+    private float dis = 1f;
+
+    
+    private Transform pointTr = null;
+    private float angle = 90f;
+    //private int hour = 0;
+    //private int min = 0;
+    private int sec = 0;
+    
+
+    private void Awake()
+    {
+        //부모 아래에서 프리팹 생성 (ex. AroundBall)
+        GameObject go_sec = Instantiate(secpPrefab);
+        //SetParent : 부모의 위치에 설정하기. (AroundBall.transform)
+        go_sec.transform.SetParent(transform);
+        //go_sec 함수 내에서 생성 되었기에 외부로 던지기 위해 pointTr이라는 transform변수에 넣는다.
+        pointTr = go_sec.transform;  
+        
+    }
+
+    private void Update()
+    {
+        //전체시간을 초 단위로 반환하기 때문에 실제 사용할 시, 분, 초로 환산
+        double dayTime = System.DateTime.Now.TimeOfDay.TotalSeconds;
+        sec = (int)(dayTime % 60.0);
+        
+        angle = -(sec * 6f);
+        if (angle > 270) angle = -90;
+
+
+        float x = Mathf.Cos((angle * Mathf.Deg2Rad));
+        float y = 0f;
+        float z = Mathf.Sin((angle * Mathf.Deg2Rad));
+
+        Vector3 target = targetTr.position;
+        pointTr.position = target + new Vector3(x, y, z) * dis;
+        
+    }
+}
